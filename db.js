698207995d58310f67624991cadea73f6f7a05eb
artitsamchooosri <33492904+artitsamchooosri@users.noreply.github.com>
2022-07-20 16:30:38 +0700

    first commit

---
+++ db.js
@@ -0,0 +1,9 @@
+const configdatabesd = {
+    user: "JIT-admin-D-4dqxzg00",
+    password: "-V-IJD-rp-8np7|1aOD_.G--M6-8Ma7nM.9S-uM9-|q#bZzY2xJlQNfk/DnGD-KQzk--NL-Jj-5j-XwR/kLfIE-R3L",
+    server: "spartan-srv-apac-d365opsprod-50c16115f8db.database.windows.net",
+    database: "db_d365opsprod_ddctest_ax_20220720_12572612_5a05" 
+};
+module.exports={configdatabesd}
+
+
